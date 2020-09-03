7cff1f0329b35398e5a9f00bbb7b9663427f61bd
liuwenfeng <846454253@qq.com>
2020-09-03 17:50:23 +0800

    20200903 下班前

---
+++ TradeTools_Main.py
@@ -0,0 +1,12 @@
+#coding=utf-8
+
+from Util import *
+from huobi import HuobiService
+
+HuobiSpot=HuobiSpot(ACCESS_KEY, SECRET_KEY, HUOBI_SERVICE_API)
+
+if __name__ == "__main__":
+    print("查询账户信息")
+    print(HuobiSpot.getAccountInfo())
+
+
---
+++ Util.py
@@ -0,0 +1,11 @@
+#coding=utf-8
+
+#在此输入您的Key
+ACCESS_KEY="8d5c98ba-2cbb1b58-d999a85f-ez2xc4vb6n"
+SECRET_KEY="72976149-f2fc4e0c-251df332-94048"
+
+HUOBI_SERVICE_API="https://api.huobi.pro"
+
+ACCOUNTS = "/v1/account/accounts"
+
+
